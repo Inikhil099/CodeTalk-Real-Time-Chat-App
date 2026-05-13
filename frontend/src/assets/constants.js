6b38981be30de9eac2ceae14c22e87ef0be59edd
Nikhil <nikhilraikwar099@gmail.com>
2026-05-13 18:00:37 +0530

    done

--- frontend/src/assets/constants.js
+++ frontend/src/assets/constants.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const viteurl = "https://entirely-consist-dedicated-apparel.trycloudflare.com";
+const viteurl = "https://commented-sets-future-opposed.trycloudflare.com";
 
 const baseURL = viteurl;
 export const backend_url = axios.create({ baseURL,withCredentials:true, });
--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -6,7 +6,7 @@ import {
 import { createContext, useContext, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
-const backend_url = "https://entirely-consist-dedicated-apparel.trycloudflare.com";
+const backend_url = "https://commented-sets-future-opposed.trycloudflare.com";
 
 const SocketContext = createContext(null);
 
